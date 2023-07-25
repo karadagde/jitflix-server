13d75c4883023b0aeaf0274c1c8bb7a9a0da65f1
Deniz.Karadag <deniz.karadag@finaps.nl>
2023-07-25 10:02:57 +0200

    applied spring sec 3.1 syntax

--- src/main/java/Jitflix/Jitflix/config/WebSecurityConfig.java
+++ src/main/java/Jitflix/Jitflix/config/WebSecurityConfig.java
@@ -5,20 +5,10 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -27,50 +17,18 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(AbstractHttpConfigurer::disable);
-//	                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().permitAll()
-//                )
-//        http
-//                .cors(c-> c.configurationSource(request -> {
-//                            CorsConfiguration conf =  new CorsConfiguration();
-//                            conf.setAllowedOrigins(List.of("http://localhost:4200"));
-//                            conf.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-//                            return conf;
-//                        }
-//                ));
 
+        http.httpBasic(withDefaults());
 
-
-
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll());
+        http.cors(c-> c.configurationSource(request -> {
+                            CorsConfiguration conf =  new CorsConfiguration();
+                            conf.setAllowedOrigins(List.of("http://localhost:4200"));
+                            conf.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+                            conf.setAllowCredentials(true);
+                            return conf;
+        }
+        ));
         return http.build();
-    }
-
-
-
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**").allowedMethods("*");
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")  // Specify the exact client origin here.
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
     }
 }
