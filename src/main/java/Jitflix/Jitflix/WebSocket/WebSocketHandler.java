b8f5a2810ac64375b9e0e3b0a7c4cff51991a549
Deniz.Karadag <deniz.karadag@finaps.nl>
2023-07-25 20:26:01 +0200

    handled cors policy

--- src/main/java/Jitflix/Jitflix/WebSocket/WebSocketHandler.java
+++ src/main/java/Jitflix/Jitflix/WebSocket/WebSocketHandler.java
@@ -3,38 +3,98 @@ package Jitflix.Jitflix.WebSocket;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class WebSocketHandler extends TextWebSocketHandler {
     private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
-//    private Map<String, Map<String, String>> storedMessages = new ConcurrentHashMap<>();
-
+    private final Map<String, Map<String, String>> storedMessages = new ConcurrentHashMap<>();
+    private String offerMessages;
+    private String answerSession;
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message)
             throws InterruptedException, IOException {
-
+        Configuration conf = Configuration.defaultConfiguration().addOptions(Option.SUPPRESS_EXCEPTIONS);
         String sessionId = session.getId();
-////        ObjectMapper objectMapper = new ObjectMapper();
-////System.out.println("message: " + message);
-////System.out.println("message payload: " + message.getPayload());
-////System.out.println("sessionId: " + session.getId());
-//
-//        Map<String, Object> messageMap = objectMapper.readValue(message.getPayload(), new TypeReference<Map<String,Object>>(){});
-////        System.out.println("messageMap: " + messageMap);
-//        Map<String, Object> type = (Map<String, Object>) messageMap.get("sdp");
-//        if(type != null){
-//            System.out.println("type: " + type.getClass());
-//            Map<String, Object> messageType = (Map<String, Object>) type.get("type");
-//    System.out.println("type: " + messageType);
-//
-//        }
+
+        // This part is for parsing the JSON message in a way that allows for null values
+        // In an ideal world I may need to add some error handling here
+        // TODO check if there is better way to handle object access
+        DocumentContext jsonContext = JsonPath.using(conf).parse(message.getPayload());
+        String sdpType = jsonContext.read("$.sdp.type", String.class);
+        String sdp = jsonContext.read("$.sdp.sdp", String.class);
+        String candidate = jsonContext.read("$.candidate.candidate", String.class);
+        String candidateSdpMid = jsonContext.read("$.candidate.sdpMid", String.class);
+        String userMessage = jsonContext.read("$.message", String.class);
+
+
+
+        while (sdpType != null){
+            if (sdpType.equals("offer")){
+                this.offerMessages=sdp;
+                session.sendMessage(message);
+                break;
+            }
+            if (sdpType.equals("answer")){
+                answerSession = sessionId;
+                if (this.offerMessages != null){
+                    session.sendMessage(new TextMessage(this.offerMessages));
+                    break;
+                }
+            }
+        }
+
+
+        if(userMessage != null){
+          int ses = this.sessions.indexOf(answerSession);
+            if (ses != -1){
+                this.sessions.get(ses).sendMessage(message);
+            }
+//            session.sendMessage(message);
+        }
+
+
+        System.out.println("message: " + message);
+        System.out.println("message payload: " + message.getPayload());
+        System.out.println("sessionId: " + session.getId());
+        System.out.println("sdpType: " + sdpType);
+        System.out.println("sdp: " + sdp);
+        System.out.println("candidate: " + candidate);
+        System.out.println("candidateSdpMid: " + candidateSdpMid);
+        System.out.println("userMessage: " + userMessage);
+
+
+
+
+
+
+
+
+
+        /*
+
+        // This part is accessing messages with Map and Optional types
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Map> requestMap = objectMapper.readValue(message.getPayload(), Map.class);
+        Optional<Map> sdpMap = Optional.ofNullable(requestMap.get("sdp"));
+
+        Optional<Object> actualType2 = sdpMap.flatMap(map -> Optional.ofNullable(map.get("type")));
+        actualType2.ifPresent(o -> System.out.println("actualType2: " + o));
+        System.out.println("messageMap2: " + requestMap);
+        */
+
 //        if ("offer".equals(type) || "candidate".equals(type)) {
 //            // Store the offer or candidate message
 //            storedMessages.computeIfAbsent(sessionId, k -> new ConcurrentHashMap<>()).put(type, message.getPayload());
@@ -43,18 +103,18 @@ public class WebSocketHandler extends TextWebSocketHandler {
         for (WebSocketSession webSocketSession : sessions) {
             if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
                 webSocketSession.sendMessage(message);
-
-                // Send any stored offer or candidate from the current session to the other sessions
-//                Map<String, String> messagesForSession = storedMessages.get(sessionId);
-//                if (messagesForSession != null) {
-//                    for (String storedType : messagesForSession.keySet()) {
-//                        String storedMessage = messagesForSession.get(storedType);
-//                        webSocketSession.sendMessage(new TextMessage(storedMessage));
-//                    }
-//                    // Clear the stored messages after sending
-//                    storedMessages.remove(sessionId);
-//                }
-
+//
+//                // Send any stored offer or candidate from the current session to the other sessions
+////                Map<String, String> messagesForSession = storedMessages.get(sessionId);
+////                if (messagesForSession != null) {
+////                    for (String storedType : messagesForSession.keySet()) {
+////                        String storedMessage = messagesForSession.get(storedType);
+////                        webSocketSession.sendMessage(new TextMessage(storedMessage));
+////                    }
+////                    // Clear the stored messages after sending
+////                    storedMessages.remove(sessionId);
+////                }
+//
             }
         }
     }
--- src/main/java/Jitflix/Jitflix/config/WebSecurityConfig.java
+++ src/main/java/Jitflix/Jitflix/config/WebSecurityConfig.java
@@ -7,8 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -17,18 +23,43 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .cors(AbstractHttpConfigurer::disable);
+//        http.httpBasic(withDefaults());
 
-        http.httpBasic(withDefaults());
-
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll());
+//        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll());
         http.cors(c-> c.configurationSource(request -> {
                             CorsConfiguration conf =  new CorsConfiguration();
                             conf.setAllowedOrigins(List.of("http://localhost:4200"));
                             conf.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
                             conf.setAllowCredentials(true);
+                            conf.setAllowedHeaders(List.of("*"));
+
                             return conf;
         }
         ));
+        http.httpBasic(withDefaults());
         return http.build();
     }
+
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedMethods("*");
+//
+//            }
+//        };
+//    }
 }
